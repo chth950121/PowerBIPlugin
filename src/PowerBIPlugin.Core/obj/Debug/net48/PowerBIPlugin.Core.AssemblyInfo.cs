41615ba0f77f6b67ff7de73758428debb635745f
chth950121 <chth950121@proton.me>
2025-03-06 17:19:22 -0500

    Optimized project.

--- src/PowerBIPlugin.Core/obj/Debug/net48/PowerBIPlugin.Core.AssemblyInfo.cs
+++ src/PowerBIPlugin.Core/obj/Debug/net48/PowerBIPlugin.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerBIPlugin.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92df687859e206c3cbc024fd5cf4fb23a0f11600")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30750de655b2ac416418b5c85f42d027f64ca2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerBIPlugin.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerBIPlugin.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
