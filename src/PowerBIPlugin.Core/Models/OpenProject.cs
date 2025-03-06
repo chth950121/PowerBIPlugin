273f9ba79cf66325e6c335d59dd1350ba4c70910
chth950121 <chth950121@proton.me>
2025-03-06 17:09:03 -0500

    finish the second milestone

--- src/PowerBIPlugin.Core/Models/OpenProject.cs
+++ src/PowerBIPlugin.Core/Models/OpenProject.cs
@@ -1,11 +1,17 @@
-namespace PowerBIPlugin
+public class OpenProject
 {
-    public class OpenProject
+    public string? ProjectName { get; set; } // Use nullable if they can be null
+    public string? FilePath { get; set; }
+    public int? Port { get; set; }
+    public string? WorkspacePath { get; set; }
+    public DateTime LastModified { get; set; }
+
+    public OpenProject(string? projectName, string? filePath, int? port, string? workspacePath, DateTime lastModified)
     {
-        public string ProjectName { get; set; }
-        public string FilePath { get; set; }
-        public string Port { get; set; }
-        public string WorkspacePath { get; set; }
-        public DateTime LastModified { get; set; }
+        ProjectName = projectName;
+        FilePath = filePath;
+        Port = port;
+        WorkspacePath = workspacePath;
+        LastModified = lastModified;
     }
 }
